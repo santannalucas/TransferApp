8563f22d6ad80f4239b8f18174ab832bb831cf7e
santannalucas <santanna.lucas@gmail.com>
2023-03-05 16:02:17 +1100

    UI init

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :accounts, only: [:index]
+  resources :transfers
 
+  root 'accounts#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
