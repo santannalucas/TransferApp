d2285683b5853f400c9507207d3a027b8bf20baf
santannalucas <santanna.lucas@gmail.com>
2023-03-07 00:00:20 +1100

    transfer controller

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,8 +1,37 @@
 class TransfersController < ApplicationController
+  require 'csv'
 
   def create
-    
+    csv_file = params[:file]
+
+    # Ensure that a CSV file was uploaded
+    if csv_file.content_type != 'text/csv'
+      flash[:error] = 'Please upload a CSV file'
+      redirect_to accounts_path and return
+    end
+
+    errors = []
+    CSV.foreach(csv_file.path, headers: false).with_index do |row, i|
+      transfer = create_transfer(row)
+      if transfer.errors.present?
+        errors << "Row #{i + 1} cannot be processed: #{transfer.errors.full_messages}"
+      end
+    end
+    Rails.logger.info(errors)
+    flash[:notice] = "CSV file uploaded successfully. #{('Errors been found:' + errors.to_sentence) if errors.present?}"
+    redirect_to accounts_path
   end
 
+  private
+
+  def create_transfer(row)
+    from_account = Account.find_by(number: row[0])
+    to_account = Account.find_by(number: row[1])
+    Transfer.create(
+      from_account: from_account,
+      to_account: to_account,
+      amount: row[2].try(:to_f)
+    )
+  end
 
 end
